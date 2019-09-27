8093614df5d074d262fd1f466474ff037e912177
wyh <wangyaoheng@ysstech.com>
2019-09-27 14:40:50 +0800

    [add-新增功能] 功能描述:添加注解推送数据 影响范围: StoryID :

--- dfas-monitor-web/src/main/java/com/win/dfas/monitor/web/controller/IndexController.java
+++ dfas-monitor-web/src/main/java/com/win/dfas/monitor/web/controller/IndexController.java
@@ -1,60 +1,59 @@
 package com.win.dfas.monitor.web.controller;
 
+import com.win.dfas.monitor.common.util.DateUtils;
+import com.win.dfas.monitor.common.util.RestfulTools;
 import com.win.dfas.monitor.engine.service.QpsService;
 import com.win.dfas.monitor.exporter.microservice.metrics.MonitorMetrics;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.win.dfas.monitor.common.util.DateUtils;
-import com.win.dfas.monitor.common.util.RestfulTools;
-
 //import com.win.dfas.monitor.common.util.RestfulTools;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 
 /**
-* 包名称：com.win.dfas.monitor.web.controller
-* 类名称：IndexController
-* 类描述：监控平台主控面板
-* 创建人：@author wangyaoheng
-* 创建时间：2019-08-07/13:31
-*/
+ * 包名称：com.win.dfas.monitor.web.controller
+ * 类名称：IndexController
+ * 类描述：监控平台主控面板
+ * 创建人：@author wangyaoheng
+ * 创建时间：2019-08-07/13:31
+ */
 @RestController
 @RequestMapping("/index")
-@Api(tags = { "监控平台主控面板交互接口" })
+@Api(tags = {"监控平台主控面板交互接口"})
 public class IndexController extends BaseController {
 
-	@Autowired
-	private QpsService qpsService;
+    @Autowired
+    private QpsService qpsService;
 
-	/** 监控平台主控面板操作对外服务*/
-	@MonitorMetrics
-	@ApiOperation(value = "监控平台主控面板操作对外服务", notes = "监控平台主控面板操作对外服务")
-	@GetMapping("/httpRequestQps")
-	public String httpRequestQps() {
-		return qpsService.getQps();
-	}
-	
-	/** 监控平台主控面板操作对外服务*/
-	@MonitorMetrics
-	@ApiOperation(value = "监控平台主控面板操作对外服务", notes = "监控平台主控面板操作对外服务")
-	@GetMapping("/httpRequestTotal")
-	public String httpRequestTotal() {
-		String url = "http://192.168.0.55:9090/api/v1/query?query=sum(http_requests_total_" + DateUtils.getCurrentDateByStringFormat()+")";
-		String result = RestfulTools.get(url, String.class);
-		return successData(result);
-	}
+    /** 监控平台主控面板操作对外服务*/
+    @MonitorMetrics
+    @ApiOperation(value = "监控平台主控面板操作对外服务", notes = "监控平台主控面板操作对外服务")
+    @GetMapping("/httpRequestQps")
+    public String httpRequestQps() {
+        return qpsService.getQps();
+    }
 
-	/** 监控平台主控面板操作对外服务2*/
-	@MonitorMetrics
-	@ApiOperation(value = "监控平台主控面板操作对外服务2", notes = "监控平台主控面板操作对外服务2")
-	@GetMapping("/httpRequestTotal2")
-	public String httpRequestTotal2() {
-		String url = "http://192.168.0.55:9090/api/v1/query?query=sum(http_requests_total_" + DateUtils.getCurrentDateByStringFormat()+")";
-		String result = RestfulTools.get(url, String.class);
-		return successData(result);
-	}
+    /** 监控平台主控面板操作对外服务*/
+    @MonitorMetrics
+    @ApiOperation(value = "监控平台主控面板操作对外服务", notes = "监控平台主控面板操作对外服务")
+    @GetMapping("/httpRequestTotal")
+    public String httpRequestTotal() {
+        String url = "http://192.168.0.55:9090/api/v1/query?query=sum(http_requests_total_" + DateUtils.getCurrentDateByStringFormat() + ")";
+        String result = RestfulTools.get(url, String.class);
+        return successData(result);
+    }
+
+    /** 监控平台主控面板操作对外服务2*/
+    @MonitorMetrics
+    @ApiOperation(value = "监控平台主控面板操作对外服务2", notes = "监控平台主控面板操作对外服务2")
+    @GetMapping("/httpRequestTotal2")
+    public String httpRequestTotal2() {
+        String url = "http://192.168.0.55:9090/api/v1/query?query=sum(http_requests_total_" + DateUtils.getCurrentDateByStringFormat() + ")";
+        String result = RestfulTools.get(url, String.class);
+        return successData(result);
+    }
 
 }
--- dfas-monitor-web/src/test/java/com/win/dfas/monitor/web/DateTest.java
+++ dfas-monitor-web/src/test/java/com/win/dfas/monitor/web/DateTest.java
@@ -1,16 +1,20 @@
 package com.win.dfas.monitor.web;
 
+import java.text.DateFormat;
+import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.junit.Test;
 
+import com.win.dfas.monitor.common.util.DateUtils;
+
 public class DateTest {
 
 	@Test
 	public void test() throws ParseException {
-		String s ="1569502775670";
+		String s = "1569502775670";
 		//String s = "1569500656042";
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		long lt = new Long(s);
@@ -21,7 +25,7 @@ public class DateTest {
 
 	@Test
 	public void test2() throws ParseException {
-		Date date=new Date();
+		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		System.out.println(sdf.format(date));
 		//获取当前时间戳,也可以是你自已给的一个随机的或是别人给你的时间戳(一定是long型的数据)
@@ -31,9 +35,65 @@ public class DateTest {
 		System.out.println(sdf.format(date));// 再将该时间转换成字符串格式
 		timeStamp = date.getTime();
 		System.out.println(timeStamp);
-		String str=String.valueOf(timeStamp);
-		str = str.substring(0,str.length()-3)+"."+str.substring(str.length()-3);
+		String str = String.valueOf(timeStamp);
+		str = str.substring(0, str.length() - 3) + "." + str.substring(str.length() - 3);
 		System.out.println(str);
+	}
+
+	@Test
+	public void changetoDouble() {
+		try {
+			double dDate = 1.569555696274E9;
+			DecimalFormat df = new DecimalFormat("#.000");
+			String strDate = df.format(dDate);
+			Date date = new Date(Long.valueOf(strDate.replace(".", "")));
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	        String strDate2 = simpleDateFormat.format(date);
+	        System.out.println(strDate2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	/**
+	 * double 转  Date 时间
+	 * @param dVal
+	 */
+	public static String doubleToDate(double dVal) {
+		Date oDate = new Date();
+		@SuppressWarnings("deprecation")
+		long localOffset = oDate.getTimezoneOffset() * 60000; //系统时区偏移 1900/1/1 到 1970/1/1 的 25569 天
+		oDate.setTime((long) ((dVal - 25569) * 24 * 3600 * 1000 + localOffset));
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return  simpleDateFormat.format(oDate);
+	}
+
+	public static double changetoDouble3(String strDate) {
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Double etDay = 0.0;
+		Double etTime = 0.0;
+		try {
+			Date date = df.parse(strDate);
+			etDay = Double.parseDouble(date.getTime() / (1000 * 60 * 60 * 24) + "");
+			etTime = date.getHours() / 24.0 + date.getMinutes() / (24.0 * 60) + date.getSeconds() / (24.0 * 60 * 60);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return etDay + etTime;
+	}
+
+	/**
+     * double 转  Date 时间
+     * @param dVal
+     */
+    public static Date DoubleToDate(Double dVal){
+    	Date oDate = new Date();
+        @SuppressWarnings("deprecation")
+		long localOffset = oDate.getTimezoneOffset() * 60000; //系统时区偏移 1900/1/1 到 1970/1/1 的 25569 天
+        oDate.setTime((long) ((dVal - 25569) * 24 * 3600 * 1000 + localOffset));
+        
+        return oDate;
 	}
 
 }
