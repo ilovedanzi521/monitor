1e7de85df4e4012032d35c2d850ae3ad208f2aca
wyh <wangyaoheng@ysstech.com>
2019-09-30 18:13:33 +0800

    [add-新增功能] 功能描述:添加首页布局 影响范围: StoryID :

--- dfas-monitor-web/src/main/frontend/src/page/home/view/index.vue
+++ dfas-monitor-web/src/main/frontend/src/page/home/view/index.vue
@@ -1,110 +1,158 @@
 <template>
-  <div class="hello">
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Home Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader home
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <div class="homeLeft">
+    <div class="platformOverview">
+      <div style="width:72px;
+           height:24px;
+           font-size:18px;
+           font-family:Microsoft YaHei;
+           font-weight:400;
+           line-height:24px;
+           color:rgba(255,255,255,1);
+           opacity:1;">平台概况
+      </div>
+    </div>
+    <div class="qps">
+      <div style="width:90px;
+height:24px;
+font-size:18px;
+font-family:Microsoft YaHei;
+font-weight:400;
+line-height:24px;
+color:rgba(255,255,255,1);
+opacity:1;">Qps趋势图
+      </div>
+    </div>
+    <div class="machineStatus">
+      <div style="width:72px;
+height:24px;
+font-size:18px;
+font-family:Microsoft YaHei;
+font-weight:400;
+line-height:24px;
+color:rgba(255,255,255,1);
+opacity:1;">机器状态</div>
+    </div>
+    </div>
+    <div class="homeRight">
+      <div class="monitorTitle">
+          <div style="width:309px;
+height:39px;
+background:linear-gradient(180deg,rgba(176,227,254,1) 0%,rgba(26,167,241,1) 100%);
+opacity:1;">自动化运维监控平台</div>
+      </div>
+    <div class="microServiceStatus">
+      <div style="width:90px;
+height:24px;
+font-size:18px;
+font-family:Microsoft YaHei;
+font-weight:400;
+line-height:24px;
+color:rgba(255,255,255,1);
+opacity:1;">微服务状态</div>
+    </div>
+      <div>
+        <div class="machineList">
+          <div style="width:72px;
+height:24px;
+font-size:18px;
+font-family:Microsoft YaHei;
+font-weight:400;
+line-height:24px;
+color:rgba(255,255,255,1);
+opacity:1;">机器列表</div>
+        </div>
+        <div class="exception">
+          <div style="width:36px;
+height:24px;
+font-size:18px;
+font-family:Microsoft YaHei;
+font-weight:400;
+line-height:24px;
+color:rgba(255,255,255,1);
+opacity:1;">异常</div>
+        </div>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script lang="ts">
   import HomeController from '../controller/HomeController';
   import Component from "vue-class-component";
+
   @Component({})
-  export default class Home extends HomeController {};
+  export default class Home extends HomeController {
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
+  .homeLeft {
+    float:left;
+    margin-left:20px
+  }
+  .homeRight {
+    float:left;
+    margin-left:30px;
+  }
+  .platformOverview {
+    width: 538px;
+    height: 194px;
+    background: rgba(9, 27, 65, 1);
+    border: 1px solid rgba(11, 37, 77, 1);
+    opacity: 1;
   }
 
-  ul {
-    list-style-type: none;
-    padding: 0;
+  .qps{
+    width:538px;
+    height:253px;
+    background:rgba(9,27,65,1);
+    border:1px solid rgba(11,37,77,1);
+    opacity:1;
   }
 
-  li {
-    display: inline-block;
-    margin: 0 10px;
+  .machineStatus {
+    width:538px;
+    height:553px;
+    background:rgba(9,27,65,1);
+    border:1px solid rgba(11,37,77,1);
+    opacity:1;
   }
 
-  a {
-    color: #42b983;
+  .monitorTitle {
+    width:1310px;
+    height:50px;
+    background:rgba(9,27,65,1);
+    border:1px solid rgba(11,37,77,1);
+    opacity:1;
+  }
+
+  .microServiceStatus {
+    width:1310px;
+    height:600px;
+    background:rgba(9,27,65,1);
+    border:1px solid rgba(11,37,77,1);
+    opacity:1;
+  }
+
+  .machineList{
+    float:left;
+    width:766px;
+    height:237px;
+    background:rgba(9,27,65,1);
+    border:1px solid rgba(11,37,77,1);
+    opacity:1;
+  }
+
+  .exception{
+    float:left;
+    margin-left:30px;
+    width:512px;
+    height:237px;
+    background:rgba(9,27,65,1);
+    border:1px solid rgba(11,37,77,1);
+    opacity:1;
   }
 </style>
