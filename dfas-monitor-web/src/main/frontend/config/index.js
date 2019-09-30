7646d11eebd347abac391b3b1dbe616b2da98ecc
wyh <wangyaoheng@ysstech.com>
2019-09-30 16:08:32 +0800

    [add-新增功能] 功能描述:添加前端基础包 影响范围: StoryID :

--- dfas-monitor-web/src/main/frontend/config/index.js
+++ dfas-monitor-web/src/main/frontend/config/index.js
@@ -12,11 +12,11 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       "/dfas-auth-center": {
-        target: "http://127.0.0.1:10018", // 你请求的第三方接口http://192.168.0.61:10018,http://192.168.0.30:10018/
+        target: "http://192.168.0.61:10018", // 你请求的第三方接口http://192.168.0.61:10018,http://192.168.0.30:10018/
         changeOrigin: true // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
       },
       "/dfbp-common-basicparameter": {
-        target: "http://127.0.0.1:11010", // 你请求的第三方接口
+        target: "http://192.168.0.61:10018", // 你请求的第三方接口
         changeOrigin: true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
         pathRewrite: {
           // 路径重写，
--- dfas-monitor-web/src/main/frontend/src/main.ts
+++ dfas-monitor-web/src/main/frontend/src/main.ts
@@ -1,11 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import filters from './mixin/filters'
+import Vue from "vue";
+import VueDND from "awe-dnd";
+import ElementUI from "element-ui";
+import "xe-utils";
+import App from "./App.vue";
+import router from "./router/index";
+import store from "./store/index";
+import "./assets/style/theme.scss";
+import "./assets/style/reset.scss";
+import "./assets/style/font.scss";
+import directives from "../src/mixin/directives";
+import "./layout/require.context";
+import "win-plus/dist/static/index.css";
+import filters from "../src/mixin/filters";
 
-Vue.config.productionTip = false
+Vue.use(VueDND);
+Vue.use(ElementUI);
+Vue.use(directives);
+
+Vue.config.productionTip = false;
+let d = process.env.NODE_ENV;
 
 Object.keys(filters).forEach(key => {
   Vue.filter(key, filters[key]);
@@ -20,9 +35,12 @@ router.beforeEach((to, from, next) => {
 });
 
 /* eslint-disable no-new */
+// tslint:disable-next-line: no-unused-expression
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
+  store,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
+
