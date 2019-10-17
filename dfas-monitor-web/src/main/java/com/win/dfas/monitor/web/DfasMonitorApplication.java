51fb7ed82711118bb45577e9d3f6a44cc1df76c8
wyh <wangyaoheng@ysstech.com>
2019-10-17 16:58:55 +0800

    [add-新增功能] 功能描述:添加eureka 影响范围: StoryID :

--- dfas-monitor-web/src/main/java/com/win/dfas/monitor/web/DfasMonitorApplication.java
+++ dfas-monitor-web/src/main/java/com/win/dfas/monitor/web/DfasMonitorApplication.java
@@ -5,6 +5,7 @@ import com.win.dfas.monitor.exporter.microservice.EnableMonitorConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,6 +13,7 @@ import java.net.URL;
 
 /*@EnableAdminServer*/
 //@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableMonitorConfig
 @EnableScheduling
 @MapperScan("com.win.dfas.monitor.config.mapper")
