fd0cb1cd4ab1d96c7941c3621fafbc2cb930740e
zengzhihua <zengzhihua@ysstech.com>
2019-10-29 15:05:36 +0800

    [modify-问题修复] 问题原因:修改提示数据 解决方法: 影响范围: BugID   :

--- dfas-monitor-web/src/main/frontend/src/components2/vue/monitor/BaseMonitor.vue
+++ dfas-monitor-web/src/main/frontend/src/components2/vue/monitor/BaseMonitor.vue
@@ -8,22 +8,37 @@
                     </div>
                     <p class="monitor-title" v-if="isShowTitle" :style="width?{'width':width}:''">{{aitem.ipAddress||aitem.microServiceAlias||aitem.microServiceName}}</p>
                     <div class="tool-container" v-if="isTool">
-                        <p v-if="aitem.ipAddress||aitem.microServiceName">
-                            <label>{{aitem.ipAddress?"ip地址:":"微服务名称:"}}</label>
-                            <span>{{aitem.ipAddress||aitem.microServiceName}}</span>
+                        <p v-if="aitem.ipAddress">
+                            <label>ip地址:</label>
+                            <span>{{aitem.ipAddress}}</span>
+                        </p>
+                        <p v-if="aitem.microServiceName">
+                            <label>微服务名称:</label>
+                            <span>{{aitem.microServiceName}}</span>
                         </p>
                         <p v-if="aitem.microServiceAlias">
                             <label>微服务描述:</label>
                             <span>{{aitem.microServiceAlias}}</span>
                         </p>
-                        <p v-if="aitem.cpuNum||aitem.warn">
-                            <label>{{aitem.cpuNum?"cpu个数：:":"日志告警数:"}}</label>
-                            <span>{{aitem.cpuNum||aitem.warn}}</span>
+                        <p v-if="aitem.cpuNum">
+                            <label>cpu个数：</label>
+                            <span>{{aitem.cpuNum}}</span>
                         </p>
-                        <p v-if="aitem.memorySize||aitem.error">
-                            <label>{{aitem.memorySize?"内存大小：":"日志错误数:"}}</label>
-                            <span>{{aitem.memorySize||aitem.error}}</span>
+
+                        <p v-if="aitem.warn">
+                            <label>日志告警数:</label>
+                            <span>{{aitem.warn}}</span>
                         </p>
+                        <p v-if="aitem.memorySize">
+                            <label>内存大小:</label>
+                            <span>{{aitem.memorySize}}</span>
+                        </p>
+
+                        <p v-if="aitem.error">
+                            <label>日志错误数:</label>
+                            <span>{{aitem.error}}</span>
+                        </p>
+
                         <p v-if="aitem.diskSize">
                             <label>磁盘总大小：</label>
                             <span>{{aitem.diskSize}}</span>
